17e7e76b76b111049f964cff1801f2ddf63b8cbb
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2022-11-14 14:57:52 +0200

    initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+APP_TITLE= 'KOMMY MAP VISUALIZATION'
+APP_SUB_TITLE='CMT PROOPTIKI 2022'
+
+def main():
+    st.set_page_config(APP_TITLE)
+    st.title(APP_TITLE)
+
+if __name__ == "__main__":
+    main()
