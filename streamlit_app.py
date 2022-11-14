147103b4e52b48907e084382500ae3e0ed598a97
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2022-11-14 15:14:26 +0200

    first test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import geopandas
 
 APP_TITLE= 'KOMMY MAP VISUALIZATION'
 APP_SUB_TITLE='CMT PROOPTIKI 2022'
@@ -6,6 +7,9 @@ APP_SUB_TITLE='CMT PROOPTIKI 2022'
 def main():
     st.set_page_config(APP_TITLE)
     st.title(APP_TITLE)
+
+    merge2= geopandas.read_file('periferies.json')
+    st.write(merge2)
 
 if __name__ == "__main__":
     main()
