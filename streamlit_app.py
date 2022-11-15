a4644ed48915a365b9617243a5eab6d09e80af7a
A-PALIOS <paliosadonis6@gmail.com>
2022-11-15 12:20:30 +0200

    antonis commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,8 @@ def main():
 
     #cm.palettes.dem
     #cm.plot_colormap(colors=cm.palettes.dem, axis_off=True)
-    cm.palettes.ndvi
+    #cm.palettes.ndvi
+    cm.plot_colormap(colors=cm.palettes.ndvi)
     # m.add_gdf(url, layer_name="Countries")
     # m.to_streamlit(width=400, height=800)
 
