de37502918be129bcf784fca820a6e0ba7615f41
A-PALIOS <paliosadonis6@gmail.com>
2022-11-15 12:11:29 +0200

    antonis commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ def main():
     #merge= geopandas.read_file('per_enotites.geojson')
     url='per_enotites.geojson'
     cm.plot_colormaps(width=12, height=0.4)
+    cm.to_streamlit(width=400, height=800)
     m = leafmap.Map(center=[50, -110], zoom=2)
     polygons = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_states.json'
     m.add_geojson(url, layer_name="Countries")
